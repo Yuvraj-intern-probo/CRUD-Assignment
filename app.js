0a43b1f2d2ed0aa1258fb5df667002847bb3c64e
Yuvraj Rautela <yuvrajrautela2002@gmail.com>
2023-01-05 13:35:49 +0530

    seccond commit

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const connection = require("./connect");
+const { fileURLToPath } = require('url');
 
 let app = express();
 app.use(bodyParser.json());
@@ -9,34 +10,103 @@ app.use(bodyParser.json());
 
 
 
-app.set("view engine", "hbs");
-app.set("views","./view");
+// app.set("view engine", "hbs");
+// app.set("views","./view");
 
 
 
 
 
 
-app.get("/", (req,res) =>{
-    res.render("index");
-});
-app.get("/create", (req,res) =>{
-    res.render("create");
-});
+// app.get("/", (req,res) =>{
+//     res.render("index");
+// });
+
+
+// app.get("/create", (req,res) =>{
+//     res.render("create");
+// });
+
+
 
 app.get("/show", (req,res) =>{
-    res.render("delete");
-});
-app.get("/update", (req,res) =>{
-    res.render("update");
-});
-app.get("/delete", (req,res) =>{
-    res.render("delete");
+    let qry = "select * from userInfo";
+    connection.query(qry, (err,results) => {
+        if(err)
+            throw err;
+        else
+        {
+            res.send(results);
+        }
+    })
 });
 
 
-app.get("/createUser", (req,res) => {
-    const {fname,lname,email,mobileno} = req.query;
+app.put("/update/:mobileno", (req,res) =>{
+    const {fname,lname,email,image} = req.body;
+
+    const {mobileno} = req.params;
+
+    let qry = "select * from userInfo where mobileno = ?";
+    connection.query(qry, [mobileno], (err,results)=>{
+        if(err)
+            throw err;
+        else
+        {
+            if(results.length > 0)
+            {
+                let insert_qry = "update userInfo set fname = ?,lname = ?,email = ?,image = ? where mobileno = ?";
+                connection.query(insert_qry, [fname,lname,email,image,mobileno], (err,results) => {
+                    if(err)
+                        throw err;
+
+                    if(results.affectedRows>0)
+                        res.send('Data updated Succesfully');
+               });
+                
+            }
+            else{
+                res.send('Entry Does not exist');
+            }
+        }
+    })
+});
+
+
+
+app.delete("/delete/:mobileno", (req,res) =>{
+    const {fname,lname,email,image} = req.body;
+
+    const {mobileno} = req.params;
+
+    let qry = "select * from userInfo where mobileno = ?";
+    connection.query(qry, [mobileno], (err,results)=>{
+        if(err)
+            throw err;
+        else
+        {
+            if(results.length > 0)
+            {
+                let insert_qry = "delete from userinfo where mobileno = ?";
+                connection.query(insert_qry, [mobileno], (err,results) => {
+                    if(err)
+                        throw err;
+
+                    if(results.affectedRows>0)
+                        res.send('Data deleted Succesfully');
+               });
+                
+            }
+            else{
+                res.send('Entry Does not exist');
+            }
+        }
+    })
+});
+
+
+app.post("/createUser", (req,res) => {
+    const {fname,lname,email,mobileno,image} = req.body;
 
 
     let qry = "select * from userInfo where Email=? or Phone=?";
@@ -50,8 +120,8 @@ app.get("/createUser", (req,res) => {
                 res.send('Data already exists');
             }
             else{
-               let insert_qry = "insert into userInfo values(?,?,?,?)";
-               connection.query(insert_qry, [fname,lname,email,mobileno], (err,results) => {
+               let insert_qry = "insert into userInfo values(?,?,?,?,?)";
+               connection.query(insert_qry, [fname,lname,email,mobileno,image], (err,results) => {
                     if(err)
                         throw err;
 
